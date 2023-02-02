0f6b227fc77fdd05d4adc3b9b7cf083ef92b0ccc
LENOVO <LENOVO@LAPTOP-3QDC6DUQ.mshome.net>
2023-02-02 17:52:10 +0530

    java assignment

---
+++ Assignment5/src/shape/shape.java
@@ -0,0 +1,37 @@
+package shape;
+public class shape extends square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		square s=new square();
+		s.getarea();
+		s.getshape();
+		circle c=new circle();
+		c.getarea();
+		c.getshape();
+		}
+
+	}
+class circle 
+{
+	public void getarea()
+	{
+		System.out.println("pai*(r*r)");
+	}
+	public void getshape()
+	{
+		System.out.println("circle have no shape it look like a egg");
+	}
+}
+class square extends circle
+{
+	public void getarea()
+	{
+		System.out.println("side*side");
+	}
+	public void getshape()
+	{
+		System.out.println("square have four equal side ");
+	}
+}
+
